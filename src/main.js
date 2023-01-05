48ed1f86ba2f10f0449aa507659c263fe2cb0610
Azad Furkan ŞAKAR <afsakarr@gmail.com>
2023-01-05 23:54:06 +0300

    Add @vercel/analytics

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import './style.css'
 import App from './App.vue'
 import router from './router';
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics';
 const pinia = createPinia()
-
+inject();
 const app = createApp(App);
 pinia.use(({ store }) => {
     store.router = markRaw(router);
